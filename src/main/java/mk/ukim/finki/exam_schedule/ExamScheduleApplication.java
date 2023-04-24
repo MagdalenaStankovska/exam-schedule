28d4ac88aac621d5ee0fddc1aa33477db57617bf
Daniela Nikolovska <daniela.nikolovska@students.finki.ukim.mk>
2023-04-24 16:53:26 +0200

    Project init

---
+++ src/main/java/mk/ukim/finki/exam_schedule/ExamScheduleApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.exam_schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamScheduleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mk/ukim/finki/exam_schedule/ExamScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.exam_schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
